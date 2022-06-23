3d8b19279e09099d0501117f3ed333995ee890ec
sabby155 <nebula155@gmail.com>
2022-06-23 13:44:56 -0400

    Combining Operators Lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         // your code here
+        boolean truthy = true;
+        boolean falsy = false;
+
+        System.out.println("Truth table for && operator:");
+        System.out.println("true && true = " + (truthy && truthy));
+        System.out.println("true && false = " + (truthy && falsy));
+        System.out.println("false && false = " + (falsy && falsy));
+        System.out.println("false && true = " + (falsy && truthy));
+        
+        System.out.println("Truth table for || operator:");
+        System.out.println("true || true = " + (truthy || truthy));
+        System.out.println("true || false = " + (truthy || falsy));
+        System.out.println("false || false = " + (falsy || falsy));
+        System.out.println("false || true = " + (falsy || truthy));
     }
 }
